d794d9e4593c87e279a9202a24296c67daa66001
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2025-12-07 18:22:12 +0100

    Add day3 / Part 2 attempt

--- Day3.cs
+++ Day3.cs
@@ -1,10 +1,54 @@
 ﻿using System.Globalization;
+using System.Text;
 using static Utils;
 
 class Day3 : Day
 {   
     protected override List<string> PuzzleInput { get; } = GetPuzzleInput(3);
+
     protected override void Solve()
+    {
+        long totalJoltage = 0;
+
+        foreach (string bank in PuzzleInput)
+        {
+            SortedDictionary<int, int> battery = new();
+
+            for (int iBattery = 0; iBattery < 12; iBattery++)
+            {
+                // Find highest digit in a onoccupied position and assign it
+                (int Index, int Value) highestDigit = default;
+                for (int i = 0; i < bank.Length; i++)
+                {
+                    // --- Skip occupied positions
+                    if (battery.ContainsKey(i)) { 
+                        continue;
+                    }
+
+                    // --- Find
+                    int digit = (int)char.GetNumericValue(bank[i]);
+                    if (digit > highestDigit.Value)
+                    {
+                        highestDigit.Index = i;
+                        highestDigit.Value = digit;
+                    }
+                }
+                // --- Assign
+                battery[highestDigit.Index] = highestDigit.Value;
+            }
+
+            StringBuilder batteryStringBuilder = new();
+            foreach (int jolt in battery.Values)
+            {
+                batteryStringBuilder.Append(jolt);
+            }
+            totalJoltage += long.Parse(batteryStringBuilder.ToString());
+        }
+
+        Solution = ("todo", totalJoltage.ToString());
+    }
+
+    protected void Solve_Old()
     {
         int totalJoltage = 0;
 
